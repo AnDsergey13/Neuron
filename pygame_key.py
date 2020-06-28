1b50f230b2848bf308032eb3d60fd4ffeb04ba64
Paciupa <65620103+Paciupa@users.noreply.github.com>
2020-06-28 22:55:55 +0300

    style: Преобразование табуляции в пробелы

--- pygame_key.py
+++ pygame_key.py
@@ -10,100 +10,100 @@ import time
 import math
 import numpy as np
 import threading, queue
-		
+        
 def pause_ses():
-	global pause_session
-	pause_session = not pause_session
-	print(pause_session)
+    global pause_session
+    pause_session = not pause_session
+    print(pause_session)
 
 def closed_window():
-	global done
-	done = False
+    global done
+    done = False
 
 def draw_num_th():
-	# сначала закрасим текст черным (типа очистки), а потом отрисуем на нём количество активных потоков
-	text1 = fontObj.render("█████", 1, (0, 0, 0),(0, 0, 0)) #alt + 219
-	window. blit(text1, (10, 100))
-	num_TH = str(threading.active_count())
-	text1 = fontObj.render(num_TH, 1, (0, 255, 0))
-	window. blit(text1, (10, 100))
+    # сначала закрасим текст черным (типа очистки), а потом отрисуем на нём количество активных потоков
+    text1 = fontObj.render("█████", 1, (0, 0, 0),(0, 0, 0)) #alt + 219
+    window. blit(text1, (10, 100))
+    num_TH = str(threading.active_count())
+    text1 = fontObj.render(num_TH, 1, (0, 255, 0))
+    window. blit(text1, (10, 100))
 
 def clear_bot(pix, x, y):
-	"""заполнение чёрным там, где был бот"""
-	pix. fill((0, 0, 0))
-	window. blit(pix, (x, y))
-	pygame. display. flip()
+    """заполнение чёрным там, где был бот"""
+    pix. fill((0, 0, 0))
+    window. blit(pix, (x, y))
+    pygame. display. flip()
 
 def keyboard_pressed(key_p, function):# клавиатура
-	"""
-	key_p - нажимаемая клавиша. клавиатура(pygame.K_SPACE)
-	function - запускаемая функция после события
-	Example. key_pressed(pygame.K_SPACE, pause_ses)
-	"""
-	global block_keyboard
+    """
+    key_p - нажимаемая клавиша. клавиатура(pygame.K_SPACE)
+    function - запускаемая функция после события
+    Example. key_pressed(pygame.K_SPACE, pause_ses)
+    """
+    global block_keyboard
 
-	if pygame.key.get_pressed()[key_p] == 1:
+    if pygame.key.get_pressed()[key_p] == 1:
 
-		if block_keyboard == False:
-			# print(block_keyboard) 
-			time.sleep(0.5)
-			block_keyboard = True
-			function()
-	else:
-		block_keyboard = False
+        if block_keyboard == False:
+            # print(block_keyboard) 
+            time.sleep(0.5)
+            block_keyboard = True
+            function()
+    else:
+        block_keyboard = False
 
 def mouse_pressed(key_p, function):
-	"""
-	key_p - нажимаемая клавиша ЛКМ(0),СКМ(1),ПКМ(2)
-	function - запускаемая функция после события
-	Example. key_pressed(0, closed)
-	"""
-	global block_mouse
+    """
+    key_p - нажимаемая клавиша ЛКМ(0),СКМ(1),ПКМ(2)
+    function - запускаемая функция после события
+    Example. key_pressed(0, closed)
+    """
+    global block_mouse
 
-	if pygame.mouse.get_pressed()[key_p] == 1:# мышь
-		if block_mouse == False:
-			# print(block_mouse)
-			block_mouse = True
-			function()
-	else:
-		block_mouse = False		
+    if pygame.mouse.get_pressed()[key_p] == 1:# мышь
+        if block_mouse == False:
+            # print(block_mouse)
+            block_mouse = True
+            function()
+    else:
+        block_mouse = False     
 
 def start_th():
-	create_bot = threading.Thread(target=create_object, args=(3,3))
-	# create_object(5,5)
-	create_bot.start()
+    create_bot = threading.Thread(target=create_object, args=(3,3))
+    # create_object(5,5)
+    create_bot.start()
 
 def create_object(x_scale = 1, y_scale = 1):
-	global LIST_BOT 
+    global LIST_BOT 
 
-	last_id = max(LIST_BOT.take(1, axis=1))
-	current_id = last_id + 1
-	LIST_BOT = np.append(LIST_BOT, np.zeros((1, num_index_list_bot), dtype=int), axis=0)
-	# print(LIST_BOT.shape)
+    last_id = max(LIST_BOT.take(1, axis=1))
+    current_id = last_id + 1
+    LIST_BOT = np.append(LIST_BOT, np.zeros((1, num_index_list_bot), dtype=int), axis=0)
+    # print(LIST_BOT.shape)
 
-	(x, y) = pygame.mouse.get_pos()
-	pix = pygame.Surface((x_scale, y_scale))
+    (x, y) = pygame.mouse.get_pos()
+    pix = pygame.Surface((x_scale, y_scale))
 
-	draw_num_th()
+    draw_num_th()
 
-	while done:
-		if pause_session == True:
-			continue
-		clear_bot(pix, x, y)
+    while done:
+        if pause_session == True:
+            continue
+        clear_bot(pix, x, y)
 
-		x += math.sin(x)*5
-		y += -math.cos(y)*5
-		# print(x,y)
+        x += math.sin(x)*5
+        y += -math.cos(y)*5
+        # print(x,y)
 
-		# если выход за пределы комнаты, то удаление
-		if x > screen_width or x < 0 or y > screen_height or y < 0:
-			clear_bot(pix, x, y)
-			draw_num_th()
-			break
+        # если выход за пределы комнаты, то удаление
+        if x > screen_width or x < 0 or y > screen_height or y < 0:
+            clear_bot(pix, x, y)
+            draw_num_th()
+            break
 
-		pix. fill((np.random.randint(140, 255), np.random.randint(140, 255), 255))
-		window. blit(pix, (x, y))
-		pygame. display. flip()
+        pix. fill((np.random.randint(140, 255), np.random.randint(140, 255), 255))
+        window. blit(pix, (x, y))
+        pygame. display. flip()
 
 num_index_list_bot = 3
 # [id, x, y]
@@ -134,43 +134,43 @@ flow_priority = queue.Queue()
 
 
 while done:
-	for e in pygame.event.get() :
-		if e. type == pygame. QUIT:
-			done = False
+    for e in pygame.event.get() :
+        if e. type == pygame. QUIT:
+            done = False
 
-	keyboard_pressed(pygame.K_SPACE, pause_ses)
-	keyboard_pressed(pygame.K_z, closed_window)
+    keyboard_pressed(pygame.K_SPACE, pause_ses)
+    keyboard_pressed(pygame.K_z, closed_window)
 
-	(x,y) = pygame.mouse.get_pos()
-	if x > 0 and x < screen_width and y > 0 and y < screen_height:
-		mouse_pressed(0, start_th)
-
-
-	if pause_session == True:
-		continue
-
-	pygame.draw.rect(window, (255, 255, 255), (45, 75, 10, 75))
-	# pygame.draw.rect(window, (64, 128, 255), (150, 20, 100, 150), 4)
-	
+    (x,y) = pygame.mouse.get_pos()
+    if x > 0 and x < screen_width and y > 0 and y < screen_height:
+        mouse_pressed(0, start_th)
 
 
+    if pause_session == True:
+        continue
 
-	pygame. display. flip()
-	pygame.time.delay(speed)
+    pygame.draw.rect(window, (255, 255, 255), (45, 75, 10, 75))
+    # pygame.draw.rect(window, (64, 128, 255), (150, 20, 100, 150), 4)
+    
+
+
+
+    pygame. display. flip()
+    pygame.time.delay(speed)
 
 
 # # генерация случайной позиции по х и по y
-# 	# random.seed(5)
-# 	x = np.random.randint(0, screen_width )
-# 	y = np.random.razdint(0, screen_height)
-# 	read_number = number_pix.take(x, axis=0)[y] # взятие числа в матрице по координатам 
-# 	number_pix[x, y] = read_number + step	# увеличение яркости цвета на step(от чёрного к белому) при повторном попадании пикселя, на одно и тоже место
-# 	if read_number > 255:
-# 		pix. fill((255, 0, 255))
-# 	else:
-# 		pix. fill((read_number, read_number, read_number))
+#   # random.seed(5)
+#   x = np.random.randint(0, screen_width )
+#   y = np.random.razdint(0, screen_height)
+#   read_number = number_pix.take(x, axis=0)[y] # взятие числа в матрице по координатам 
+#   number_pix[x, y] = read_number + step   # увеличение яркости цвета на step(от чёрного к белому) при повторном попадании пикселя, на одно и тоже место
+#   if read_number > 255:
+#       pix. fill((255, 0, 255))
+#   else:
+#       pix. fill((read_number, read_number, read_number))
 
-# 	window. blit(pix, (x, y))
+#   window. blit(pix, (x, y))
 
 
 #1
