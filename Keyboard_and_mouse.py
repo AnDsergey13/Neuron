2d965d0bb7de4a366a21c9310d77a7f3a89336a0
Anakin <44977098+AnD13@users.noreply.github.com>
2020-08-07 22:40:39 +0300

    refactor: возврат к предыдущей стабильной версии

--- Keyboard_and_mouse.py
+++ Keyboard_and_mouse.py
@@ -8,12 +8,8 @@ class Event_mouse_and_keyboard():
 		"""  Стартовые параметры """
 		pass
 
-	def pressed(self, key_p, function):
+	def keyboard_pressed(self, key_p, function):
 		"""
-		key_p - нажимаемая клавиша ЛКМ(0),СКМ(1),ПКМ(2)
-		function - запускаемая функция после события
-		Example. key_pressed(0, closed)
-
 		key_p - нажимаемая клавиша. клавиатура(pygame.K_SPACE)  # Стиль имени!
 		function - запускаемая функция после события
 		Example. key_pressed(pygame.K_SPACE, pause_ses)  # Стиль имени!
@@ -21,57 +17,26 @@ class Event_mouse_and_keyboard():
 
 		if pygame.key.get_pressed()[key_p] == 1:
 
-			if not block:
+			if not Event_mouse_and_keyboard.block:
 				# print(block_keyboard)
 				time.sleep(0.5)
-				block = True
+				Event_mouse_and_keyboard.block = True
 				function()
 		else:
-			block = False
+			Event_mouse_and_keyboard.block = False
 
 
 	def mouse_pressed(self, key_p, function):
+		"""
+		key_p - нажимаемая клавиша ЛКМ(0),СКМ(1),ПКМ(2)
+		function - запускаемая функция после события
+		Example. key_pressed(0, closed)
+		"""
 
 		if pygame.mouse.get_pressed()[key_p] == 1:  # мышь
-			if not block:
+			if not Event_mouse_and_keyboard.block:
 				# print(block_mouse)
-				block = True
+				Event_mouse_and_keyboard.block = True
 				function()
 		else:
-			block = False
-
-
-	# def keyboard_pressed(self, key_p, function):
-	# 	"""
-	# 	key_p - нажимаемая клавиша. клавиатура(pygame.K_SPACE)  # Стиль имени!
-	# 	function - запускаемая функция после события
-	# 	Example. key_pressed(pygame.K_SPACE, pause_ses)  # Стиль имени!
-	# 	"""
-	# 	global block_keyboard
-
-	# 	if pygame.key.get_pressed()[key_p] == 1:
-
-	# 		if not block:
-	# 			# print(block_keyboard)
-	# 			time.sleep(0.5)
-	# 			block = True
-	# 			function()
-	# 	else:
-	# 		block = False
-
-
-	# def mouse_pressed(self, key_p, function):
-	# 	"""
-	# 	key_p - нажимаемая клавиша ЛКМ(0),СКМ(1),ПКМ(2)
-	# 	function - запускаемая функция после события
-	# 	Example. key_pressed(0, closed)
-	# 	"""
-	# 	global block
-
-	# 	if pygame.mouse.get_pressed()[key_p] == 1:  # мышь
-	# 		if not block:
-	# 			# print(block_mouse)
-	# 			block = True
-	# 			function()
-	# 	else:
-	# 		block = False
+			Event_mouse_and_keyboard.block = False
