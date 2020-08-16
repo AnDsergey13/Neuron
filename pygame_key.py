77a70cbcf533123b17568c9a28b5a150ce535f6f
Anakin <44977098+AnD13@users.noreply.github.com>
2020-08-16 13:13:31 +0300

    chore : исправлена отрисовка количества ботов

--- pygame_key.py
+++ pygame_key.py
@@ -93,8 +93,8 @@ while done:
 	label_num_th.clear_text()
 	bool_mouse.clear_text()
 	py.update_screen()
-	# рисуем количество потоков
-	label_num_th.draw_text(threading.active_count())
+	# рисуем количество ботов на экране. Не считаем главный поток, поэтому минус 1
+	label_num_th.draw_text(threading.active_count()-1)
 	# Рисуем позицию мыши
 	bool_mouse.draw_text(py.get_pos_mouse())
 
