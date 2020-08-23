4d2e7c19aec0488f051eed29d5f93d8a938b443f
Anakin <44977098+AnD13@users.noreply.github.com>
2020-08-23 10:27:20 +0300

    style : Добавлены пробелы для красоты

--- Neuron.py
+++ Neuron.py
@@ -7,6 +7,7 @@ import numpy as np
 
 class Neuron_(Drawing):
 	""" Класс для работы с ботами """
+
 	def __init__(self, mode=1):
 		""" mode = 0 - без потоков 
 			mode = 1 - это когда один бот в одном потоке 
@@ -14,11 +15,13 @@ class Neuron_(Drawing):
 		if mode == 1:
 			create_th = threading.Thread(target=self.create_bot)
 			create_th.start()
+
 	def create_bot(self):
 		""" Промежуточный метод в котором создаётся бот """
 		self.x_bot, self.y_bot = self.get_pos_mouse()
 		self.neuron = Bot()
 		self.loop()	
+
 	def loop(self):
 		""" Цикл в котором всё происходит """
 		while on:
@@ -26,17 +29,21 @@ class Neuron_(Drawing):
 				continue
 			self.neuron.clear_pos_bot()
 			self.neuron.draw_bot(self.x_bot, self.y_bot)
+
 			# Метод для описания поведения движения
 			self.movement()
+
 			# если выход за пределы комнаты, то удаление
 			if not self.check_object_on_screen((self.x_bot, self.y_bot)):
 				self.neuron.clear_pos_bot()
 				break
+
 			# Меняем случайно цвет бота 
 			self.neuron.set_color_bot(np.random.randint(140, 255), np.random.randint(140, 255), 255)
 			
 			self.neuron.draw_bot(self.x_bot, self.y_bot)
 			self.update_screen()
+
 	def movement(self):
 		""" В этом методе описано движение бота """
 		self.x_bot += math.sin(self.x_bot) * 5
