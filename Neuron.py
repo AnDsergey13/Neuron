fc549e13f9421eaa5c07cf5cf92140545c03e1e7
Anakin <44977098+AnD13@users.noreply.github.com>
2020-08-23 10:12:25 +0300

    chore : Создали основные методы

--- Neuron.py
+++ Neuron.py
@@ -4,3 +4,9 @@ import pygame
 import math
 import threading
 import numpy as np
+
+class Neuron_(Drawing):
+	def __init__(self, mode=1):
+	def create_bot(self):
+	def loop(self):
+	def movement(self):
