8671f88442c933881161227774238d68aa71c927
Anakin <44977098+AnD13@users.noreply.github.com>
2020-08-23 10:14:17 +0300

    doc : Добавлено описание для методов

--- Neuron.py
+++ Neuron.py
@@ -6,7 +6,14 @@ import threading
 import numpy as np
 
 class Neuron_(Drawing):
+	""" Класс для работы с ботами """
 	def __init__(self, mode=1):
+		""" mode = 0 - без потоков 
+			mode = 1 - это когда один бот в одном потоке 
+		"""
 	def create_bot(self):
+		""" Промежуточный метод в котором создаётся бот """
 	def loop(self):
+		""" Цикл в котором всё происходит """
 	def movement(self):
+		""" В этом методе описано движение бота """
