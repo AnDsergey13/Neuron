9fd6caaf58814e7d74b5a5aad03d94198049d510
AnDsergey13 <https://github.com/AnDsergey13/Neuron>
2020-11-22 23:01:49 +0300

    refactor: Переписан модуль

--- Keyboard_and_mouse.py
+++ Keyboard_and_mouse.py
@@ -1,58 +1,14 @@
-import pygame
+import keyboard
+import mouse
 
 class Event():
 	""" Класс для работы с клавиатурой и мышью """
-	key_object = {
-		# Левая кнопка мыши
-		"lbm" : 0,
-		# Средняя кнопка мыши
-		"mbm" : 1,
-		# Средняя кнопка мыши
-		"rbm" : 2,
-		"space" : pygame.K_SPACE,
-		"0" : pygame.K_0,
-		"1" : pygame.K_1,
-		"2" : pygame.K_2,
-		"3" : pygame.K_3,
-		"4" : pygame.K_4,
-		"5" : pygame.K_5,
-		"6" : pygame.K_6,
-		"7" : pygame.K_7,
-		"8" : pygame.K_8,
-		"9" : pygame.K_9,
-		"a" : pygame.K_a,
-		"b" : pygame.K_b,
-		"c" : pygame.K_c,
-		"d" : pygame.K_d,
-		"e" : pygame.K_e,
-		"f" : pygame.K_f,
-		"g" : pygame.K_g,
-		"h" : pygame.K_h,
-		"i" : pygame.K_i,
-		"j" : pygame.K_j,
-		"k" : pygame.K_k,
-		"l" : pygame.K_l,
-		"m" : pygame.K_m,
-		"n" : pygame.K_n,
-		"o" : pygame.K_o,
-		"p" : pygame.K_p,
-		"q" : pygame.K_q,
-		"r" : pygame.K_r,
-		"s" : pygame.K_s,
-		"t" : pygame.K_t,
-		"u" : pygame.K_u,
-		"v" : pygame.K_v,
-		"w" : pygame.K_w,
-		"x" : pygame.K_x,
-		"y" : pygame.K_y,
-		"z" : pygame.K_z
-	}
 
 	def __init__(self, key_p):
 		"""  Стартовые параметры """
 		self.block = False
 		try:
-			self.key_p = Event.key_object.get(key_p)
+			self.key_p = key_p
 		except:
 			print(""" 
 				Ошибка ввода! Нет такой клавиши.
@@ -66,7 +22,7 @@ class Event():
 		Example. key_pressed(function)
 		"""
 
-		if pygame.key.get_pressed()[self.key_p] == 1:
+		if keyboard.is_pressed(self.key_p):
 
 			if not self.block:
 				self.block = True
@@ -79,7 +35,7 @@ class Event():
 		Возвращает True если была нажата клавиша на клавиатуре. 
 		"""
 
-		if pygame.key.get_pressed()[self.key_p] == 1:
+		if keyboard.is_pressed(self.key_p):
 
 			if not self.block:
 				self.block = True
@@ -95,7 +51,7 @@ class Event():
 		Example. mouse_pressed(function)
 		"""
 
-		if pygame.mouse.get_pressed()[self.key_p] == 1:  # мышь
+		if mouse.is_pressed(button=self.key_p):  # мышь
 			if not self.block:
 				self.block = True
 				function()
@@ -108,16 +64,15 @@ class Event():
 		Возвращает True если была нажата клавиша на мыши. 
 		"""
 
-		if pygame.mouse.get_pressed()[self.key_p] == 1:  # мышь
+		if mouse.is_pressed(button=self.key_p):  # мышь
 			if not self.block:
 				self.block = True
 				return True
 		else:
 			self.block = False
 			return False
-
-	def get_list_key(self):
-		""" Возвращает список зарезервированных клавиш """
-		return list(Event.key_object.keys())
-
+	# del ?
+	# def get_list_key(self):
+	#	""" Возвращает список зарезервированных клавиш """
+	#	return list(Event.key_object.keys())
 
