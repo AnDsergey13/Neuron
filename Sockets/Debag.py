0783011a4f4d3db3187e07cc54053fea2dba56a9
Anakin <44977098+AnDsergey13@users.noreply.github.com>
2020-12-26 01:00:37 +0300

    refactor(socket): Изменён файл для передачи команд

--- Sockets/Debag.py
+++ Sockets/Debag.py
@@ -1,1 +1,17 @@
-pass
+import Sockets
+
+
+def input_keyboard():
+	while True:
+		message = bytes(input("Введите сообщение: "), 'utf-8')
+
+		if message == b"close":
+			Sockets.set_command(message)
+			print("!!! Клавиатура отключена !!!")
+			break
+		else: 
+			Sockets.set_command(message)
+			print(f"***Сообщение {message} отправлено")
+
+
+input_keyboard()	
