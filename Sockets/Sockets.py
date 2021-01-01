ed3bccd90e66781fad9e75e2b0c312554ab91eca
Anakin <44977098+AnD13@users.noreply.github.com>
2021-01-02 01:31:06 +0300

    feature(socket, async): Создан первый рабочий асинхронный код

--- Sockets/Sockets.py
+++ Sockets/Sockets.py
@@ -10,10 +10,12 @@ Fatal Python error: could not acquire lock for <_io.BufferedWriter name='<stdout
 4) реализовать общий класс(?) для клиента и сервера
 '''
 
+'''
 # Сюда приходят команды из Debag.py(клавиатуры)
 async def set_command(command):
 	global message
 	message = command
+'''
 
 def shutdown_all_clients():
 	# Отключение всех клиентов на стороне сервера
@@ -22,16 +24,26 @@ def shutdown_all_clients():
 			client.close()
 		except:
 			print("shutdown_all_clients. Один из клиентов не может закрыться \n")
-		
-# ?????
-async def client_send_message(server, conn# ?????):
+
+
+async def client_send_message(loop, server, client):
 	global work_server
 	global message
 
+	for i in range(300):
+		await loop.sock_sendall(client, str(i).encode())
+		await asyncio.sleep(0.1)
+	clients.remove(client)
+	await loop.sock_sendall(client, b"end")
+	client.close()
+	print("Отключаем клиента \n")
+
+	"""
 	while work_server:
+		
 		if not message in list_command:
 			try:
-				await conn.send(message)
+				await loop.sock_send(client, message)
 			except:
 				print("client_send_message. Ошибка отправки сообщения \n")
 				# Отключаем соединение для всех клиентов
@@ -42,49 +54,51 @@ async def client_send_message(server, conn# ?????):
 			message = b""
 
 		if message == b"end":
-			await conn.send(message)
+			await loop.sock_send(client, message)
 			# Очищаем сообщение, чтобы условие больше не сработало
 			message = b""
 			# Удаляем из списка клиентов
-			clients.remove(conn)
-			conn.close()
+			clients.remove(client)
 			print("Отключаем клиента \n")
 			# Без брейка поток продолжит отправлять данные на закрытый сокет
 			break
 		
-# ?????
-async def server_accept(server# ?????):
+	client.close()"""
+
+
+async def server_accept(loop, server):
 	global work_server
 
 	while work_server:
-		print("Сервер слушает ... ...")
-		conn, _ = await server.accept()
-		print(f"Подключился {conn}")
-		clients.append(conn)
+		client, _ = await loop.sock_accept(server)
+		print(f"Подключился {client}")
+		loop.create_task(client_send_message(loop, server, client))
+
+		clients.append(client)
 
 
-def create_server(HOST, PORT, NUM_CLIENTS):
+def create_server():
 	global work_server
 
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server.bind((HOST, PORT))
 		server.listen(NUM_CLIENTS)
 		server.setblocking(False)
 		print("*** Сервер создан! ***")
-		server_accept(server)
 	except:
-		work_server = False
-		print("create_server. Ошибка при создании сервера \n")
+		print("create_server. Ошибка при настройке сервера \n")
 		server.shutdown(socket.SHUT_RDWR)
 		server.close()
 
 	try:
-		# ?????
 		loop = asyncio.get_event_loop()
-		loop.run_until_complete(run_server())
+		loop.run_until_complete(server_accept(loop, server))
 	except:
+		work_server = False
 		print("create_server. Ошибка при создании списка событий \n")
+		server.shutdown(socket.SHUT_RDWR)
+		server.close()
 
 
 HOST = "127.0.0.1"
@@ -96,4 +110,5 @@ work_server = True
 message = b""
 list_command = [b"end", b"close", b""]
 
-create_server(HOST, PORT, NUM_CLIENTS)
+if __name__ == "__main__":
+	create_server()
