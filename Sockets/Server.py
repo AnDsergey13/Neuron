bdf009cdc3db2d3023c824be3cf1fafee1d7f5cb
Anakin <44977098+AnD13@users.noreply.github.com>
2021-01-03 12:41:08 +0300

    feature(socket): создана заготовка для класса сервера

---
+++ Sockets/Server.py
@@ -0,0 +1,27 @@
+""" DOC """
+import socket
+import asyncio
+
+
+class Server:
+	"""	DOC	"""
+	host = "127.0.0.1"
+	port = 13254
+	number_client = 10
+	clients = []
+	data = ""
+
+	def __init__(self):
+		pass
+
+	def create_server(self):
+		""" DOC """
+		pass
+
+	def set_number_client(self):
+		""" DOC """
+		pass
+
+	def get_number_client(self):
+		""" DOC """
+		pass
