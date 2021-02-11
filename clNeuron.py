daf2907e994e661f110292ba6603bfa75d4018cf
Anakin <44977098+AnDsergey13@users.noreply.github.com>
2021-02-12 00:36:26 +0300

    feature(neuron):
    Добавлена функция для проверки нахождения нейрона в пространстве.
    
    Добавлен метод для получения внутренних точек в прострастве.
    
    Расширен метод для установки точек в пространстве.

--- clNeuron.py
+++ clNeuron.py
@@ -1,27 +1,38 @@
 import numpy as np
 
 class Neuron:
-    # Координаты вершин тэтрадра
-    # Пример структуры [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    space = np.zeros((4, 3))
+    # Координаты вершин куба
+    # Пример структуры [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    space_in = np.zeros((8, 3))
+    space_out = np.zeros((8, 3))
 
     def __init__(self, xyz_pos):
-        self.list_input = []
-        self.list_output = []
+        #self.list_input = []
+        #self.list_output = []
         self.x_pos = xyz_pos[0]
         self.y_pos = xyz_pos[1]
         self.z_pos = xyz_pos[2]
 
     @staticmethod
-    def set_point_space(points):
-        Neuron.space = points
+    def set_point_space(points, th=10):
+        """ 1 аргумент - точки геометрической фигуры (пространства).
+            2 аргумент - толщина внешней области. По умолчанию 10"""
+        Neuron.space_out = points
+        points_in = points.copy()
+        points_in = np.where(points_in == points_in.min(), points_in + th, points_in)
+        points_in = np.where(points_in == points_in.max(), points_in - th, points_in)
+        Neuron.space_in = points_in
 
     @staticmethod
-    def get_point_space():
-        return Neuron.space
+    def get_point_out_space():
+        return Neuron.space_out
+
+    @staticmethod
+    def get_point_in_space():
+        return Neuron.space_in
 
     def set_pos(self):
-        offset = 2
+        offset = 20
         self.x_pos = self.x_pos + offset
         self.y_pos = self.y_pos + offset
         self.z_pos = self.z_pos + offset
@@ -29,22 +40,27 @@ class Neuron:
     def get_pos(self):
         return self.x_pos, self.y_pos, self.z_pos
 
+    def is_internal_space(self):
+        """ Возвращает True, если нейрон находится во внутренней зоне пространства.
+            А False, когда во внешней зоне."""
+
+        in_space = self.get_point_in_space()
+        if (self.z_pos > in_space[0][2] and self.z_pos < in_space[1][2]) and (self.y_pos > in_space[0][1] and self.y_pos < in_space[4][1]) and (self.x_pos > in_space[0][0] and self.x_pos < in_space[2][0]):
+            return True
+        else:
+            return False
 
 
 
 
-#print(Neuron.get_point_space())
-Neuron.set_point_space(np.array([[157, 171, 0], [0, -16, 0], [230, -50, 75], [75, 81, 212]]))
-#print(Neuron.get_point_space())
-a = Neuron([5, 10, -7])
 
 
 
-
-
-
-
-
+#print(Neuron.get_point_out_space())
+#print(Neuron.get_point_in_space())
+#Neuron.set_point_space(np.array([[0, 0, 0], [0, 0, 99], [99, 0, 99], [99, 0, 0],[0, 99, 0], [0, 99, 99], [99, 99, 99], [99, 99, 0]]))
+#print(Neuron.get_point_out_space())
+#print(Neuron.get_point_in_space())
 
 
 
