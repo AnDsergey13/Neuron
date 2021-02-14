d90f148ffbe990ca3b8fffa65ba7001bb7cc9a39
Anakin <44977098+AnDsergey13@users.noreply.github.com>
2021-02-14 20:36:34 +0300

    feature(neuron): Добавлен класс для работы с пространством.
    Частично задокументирован код.
    
    Добавлена функция для генерации кубического пространства.

--- clNeuron.py
+++ clNeuron.py
@@ -1,35 +1,43 @@
 import numpy as np
 
-class Neuron:
-    # Координаты вершин куба
-    # Пример структуры [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
-    space_in = np.zeros((8, 3))
-    space_out = np.zeros((8, 3))
+class Space:
+    def __init__(self, number_points=8):
+        """ number_points - количество точек (вершин) у пространства.
+                По умолчанию геометрическая фигура куб (8 точек) """
+        self.in_points = np.zeros((number_points, 3))
+        self.out_points = np.zeros((number_points, 3))
 
-    def __init__(self, xyz_pos):
-        #self.list_input = []
-        #self.list_output = []
-        self.x_pos = xyz_pos[0]
-        self.y_pos = xyz_pos[1]
-        self.z_pos = xyz_pos[2]
-
-    @staticmethod
-    def set_point_space(points, th=10):
-        """ 1 аргумент - точки геометрической фигуры (пространства).
-            2 аргумент - толщина внешней области. По умолчанию 10"""
-        Neuron.space_out = points
+    def set_points(self, points, th=10):
+        """ Задаются точки для внешней области пространства. Генерируется точки для внутренней области.
+                points - массив внешних(!) точек.
+                    Пример записи для куба [[0, 0, 0], [0, 0, 99], [99, 0, 99], [99, 0, 0],[0, 99, 0], [0, 99, 99], [99, 99, 99], [99, 99, 0]]
+                th - толщина внешней области пространства. По умолчанию 10 """
+        self.out_points = points
         points_in = points.copy()
         points_in = np.where(points_in == points_in.min(), points_in + th, points_in)
         points_in = np.where(points_in == points_in.max(), points_in - th, points_in)
-        Neuron.space_in = points_in
+        self.in_points = points_in
 
-    @staticmethod
-    def get_point_out_space():
-        return Neuron.space_out
+    def get_out_points(self):
+        """ Возвращает массив точек (координат). Из внешней области пространства"""
+        return self.out_points
 
-    @staticmethod
-    def get_point_in_space():
-        return Neuron.space_in
+    def get_in_points(self):
+        """ Возвращает массив точек (координат). Из внутренней области пространства"""
+        return self.in_points
+
+    def create_cube(self, size=100):
+        """ Создаёт и возвращает массив точек для куба.
+                size(int) - размер грани """
+        return np.array([[0, 0, 0], [0, 0, size - 1], [size - 1, 0, size - 1], [size - 1, 0, 0],[0, size - 1, 0], [0, size - 1, size - 1], [size - 1, size - 1, size - 1], [size - 1, size - 1, 0]])
+
+class Neuron():
+    def __init__(self, x, y, z):
+        #self.list_input = []
+        #self.list_output = []
+        self.x_pos = x
+        self.y_pos = y
+        self.z_pos = z
 
     def set_pos(self):
         offset = 20
@@ -44,7 +52,7 @@ class Neuron:
         """ Возвращает True, если нейрон находится во внутренней зоне пространства.
             А False, когда во внешней зоне."""
 
-        in_space = self.get_point_in_space()
+        in_space = space.get_in_points()
         if (self.z_pos > in_space[0][2] and self.z_pos < in_space[1][2]) and (self.y_pos > in_space[0][1] and self.y_pos < in_space[4][1]) and (self.x_pos > in_space[0][0] and self.x_pos < in_space[2][0]):
             return True
         else:
@@ -53,14 +61,22 @@ class Neuron:
 
 
 
+# space = Space()
+# out_p = space.create_cube(50)
+# space.set_points(out_p, 15)
+# print(space.get_out_points())
+# print(space.get_in_points())
+# a = Neuron(3, 10, 13)
+# print(a.get_pos())
+# print(a.is_internal_space())
+# a.set_pos()
+# print(a.get_pos())
+# print(a.is_internal_space())
 
 
 
-#print(Neuron.get_point_out_space())
-#print(Neuron.get_point_in_space())
-#Neuron.set_point_space(np.array([[0, 0, 0], [0, 0, 99], [99, 0, 99], [99, 0, 0],[0, 99, 0], [0, 99, 99], [99, 99, 99], [99, 99, 0]]))
-#print(Neuron.get_point_out_space())
-#print(Neuron.get_point_in_space())
+
+
 
 
 
