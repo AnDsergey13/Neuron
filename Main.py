a5bf63ba10463254891b3e9c9c721833ea7495f9
AnDsergey13 <eXRxaGp5cmY=>
2021-03-07 18:43:12 +0300

    chore(main): Временно закомментированы неиспользуемые строки

--- Main.py
+++ Main.py
@@ -9,17 +9,24 @@ import numpy as np
 
 
 
-size_x = 10
-size_y = 10
 
-c = Event("c")
 
-# создаём поле
-pole = np.ones((size_x, size_y), int)
-# Заполняем поле нулями
-pole.fill(0)
 
 while not keyboard.is_pressed("space"):
 	pass
 	if c.is_keyboard_pressed():
 		print(str(pole))
+# size_x = 10
+# size_y = 10
+
+# c = Event("c")
+
+# # создаём поле
+# pole = np.ones((size_x, size_y), int)
+# # Заполняем поле нулями
+# pole.fill(0)
+
+# while not keyboard.is_pressed("space"):
+# 	pass
+# 	if c.is_keyboard_pressed():
+# 		print(str(pole))
