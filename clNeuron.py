164712e76b38a5a299841126eae6a32276293023
Anakin <44977098+AnD13@users.noreply.github.com>
2021-04-08 19:34:32 +0300

    refactor(neuron): Надпись приведена в читаемый вид

--- clNeuron.py
+++ clNeuron.py
@@ -147,5 +147,8 @@ class Neuron():
 			А False, когда во внешней зоне."""
 
 		in_space = self.obj_space.get_in_points()
-		return (self.z_pos > in_space[0][2] and self.z_pos < in_space[1][2]) and (self.y_pos > in_space[0][1] and self.y_pos < in_space[4][1]) and (self.x_pos > in_space[0][0] and self.x_pos < in_space[2][0])
+		delta_x = self.x_pos > in_space[0][0] and self.x_pos < in_space[2][0]
+		delta_y = self.y_pos > in_space[0][1] and self.y_pos < in_space[4][1]
+		delta_z = self.z_pos > in_space[0][2] and self.z_pos < in_space[1][2]
+		return delta_x and delta_y and delta_z
 
