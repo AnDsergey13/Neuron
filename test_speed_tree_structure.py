31f63f82f5e15c2f0a64f48c21ef0744474d66cc
Anakin <44977098+AnD13@users.noreply.github.com>
2021-08-26 02:04:20 +0300

    chore, feature: Оптимизация пространства. + Эксперименты Добавлена функция генерации строки Добавдена функция обрезания строки согласно глубине списка
    Проблема. Собрать весь код воедино

--- test_speed_tree_structure.py
+++ test_speed_tree_structure.py
@@ -1,21 +1,6 @@
 import time
 import numpy as np
-import copy
 
-gen_obj = 14756
-
-list_divisors = [div for div in range (1, gen_obj // 2 + 1) if gen_obj % div == 0] + [gen_obj]
-# print(list_divisors)
-
-# start_time = time.time()
-# end_time = time.time()
-# delta = end_time - start_time
-# print(delta)
-
-rule = [2, 3, 4, 2]
-
-tree = []
-list_name_var = []
 
 def gen_var(num_var):
 	""" Генерим необходимое количество переменных с именами а1, а2, а3 ..."""
@@ -27,45 +12,112 @@ def gen_var(num_var):
 def get_name_var(index):
 	return list_name_var[index]
 
-def gen_tree(lvl_deep):
-	# print(eval(get_name_var(lvl_deep)))
-	x = eval(get_name_var(lvl_deep))
-	for x in range(rule[lvl_deep]):
-		#print(x)
-		pass
+def create_all_string(name_list, deep):
+	""" Этот метод создаёт строку типа tree[a1][a2][a3]...
+		Количество [] зависит от глубины списка. 
+	"""
+
+	# создаём основу строки для вставки в неё имён переменных
+	# при глубине 4 будет tree[][][]
+	# при глубине 2 будет tree[]
+	string = name_list + "[]" * (deep - 1)
+
+	# достаём имена переменных и вставляем с основную строку 
+	for num_var in list_name_var[:-1]:
+		string = string.replace("[]", f"[{num_var}]", 1)
+	return string
+
+def get_string(string, deep):
+	# модифицируем строку, стобы проще было считать
+	string += "["
+
+	# Находим индекс [ , согласно глубине списка
+	index = 0
+	for i in range(deep):
+		index = string.find('[', index + 1)
+
+	# возвращаем обрезанную строку
+	return string[:index]
 
 
+rule = [2, 3, 4, 2]
+tree = []
+list_name_var = []
 
 deep = len(rule)
+# генерим необходимое количество переменных, и список имён этих перменных
 gen_var(deep)
+# создадим сначала полную строку, а после при необходимости будем её обрезать
+string = create_all_string("tree", deep)
 
-for lvl_deep in range(deep):
-	gen_tree(lvl_deep)
-# print(list_name_var)
-# print(globals())
-
-aa = 3
-bb = copy.deepcopy(aa)
-print(aa, bb)
-aa = 4
-print(aa, bb)
+it = [1 for i in range(deep)]
 
 
+for num_it in rule:
+	while it[num_it] < rule[num_it]:
+		# получаем обрезанную строку с необходимыми переменными
+		# s = get_string(string, num_it + 1)
+		# eval(s).append([])
+		it[num_it] += 1
+		print(it)
+
+
+
+# print(tree)
+
+# for a in range(rule[0]):
+# 	tree.append([])
+
+# 	for b in range(rule[1]):
+# 		tree[a].append([])
+
+# 		for c in range(rule[2]):
+# 			tree[a][b].append([])
+
+# 			for d in range(rule[3]):
+# 				tree[a][b][c].append([])
+
+# print(tree)
+
+# for lvl_deep in range(deep):
+# 	print(lvl_deep)
+# 	gen_tree(lvl_deep)
+# # print(list_name_var)
+# # print(globals())
+	
+
+# for num_it in range(len(rule)):
+# 	while it[num_it] < rule[num_it]:
+# 		# получаем обрезанную строку с необходимыми переменными
+#		# s = get_string(string, num_it + 1)
+#		# eval(s).append([])
+# 		it[num_it] += 1
+# 		print(it)
+
+
+# for num_vertical_it in rule:
+
+# 	for num_horizontal_it in range(1, num_vertical_it + 1):
+
+# 		print(num_vertical_it, num_horizontal_it)
+# 		# it[num_it] += 1
+# 		# print(it)
+
+
+# def gen_tree(lvl_deep):
+# 	# print(eval(get_name_var(lvl_deep)))
+# 	x = eval(get_name_var(lvl_deep))
+# 	for x in range(rule[lvl_deep]):
+# 		# print(x)
+# 		pass
+
+# aa = [3]
+# bb = aa
+# print(aa, bb)
+# aa[0] = 4
+# print(aa, bb)
+	
 '''
-for a in range(rule[0]):
-	tree.append([])
-
-	for b in range(rule[1]):
-		tree[a].append([])
-
-		for c in range(rule[2]):
-			tree[a][b].append([])
-
-			for d in range(rule[3]):
-				tree[a][b][c].append([])
-
-print(tree)	
-
 eval("tree[0][1]").append('!!!')
 print(tree)
 
@@ -97,3 +149,16 @@ def gen_loop(string):
 	for b in range(rule[1]):
 		eval(string).append([])
 '''
+
+"""
+
+gen_obj = 14756
+
+list_divisors = [div for div in range (1, gen_obj // 2 + 1) if gen_obj % div == 0] + [gen_obj]
+# print(list_divisors)
+
+# start_time = time.time()
+# end_time = time.time()
+# delta = end_time - start_time
+# print(delta)s
+"""
